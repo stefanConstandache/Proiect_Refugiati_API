d17cbad9003d26d85ded826d5c378f61e6436660
Mihnea Duican <mduican@adobe.com>
2022-04-30 17:12:24 +0300

    Init

---
+++ src/main/java/com/proiect_refugiati_api/ProiectRefugiatiApiApplication.java
@@ -0,0 +1,13 @@
+package com.proiect_refugiati_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProiectRefugiatiApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProiectRefugiatiApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/proiect_refugiati_api/ProiectRefugiatiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proiect_refugiati_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProiectRefugiatiApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
