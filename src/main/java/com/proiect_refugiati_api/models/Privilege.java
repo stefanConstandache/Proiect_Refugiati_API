5b1201387d42814394a16f9f13bae38d43c44dd2
Andra Serban <serban_andra96@yahoo.com>
2022-04-30 18:47:51 +0300

    Add models

---
+++ src/main/java/com/proiect_refugiati_api/models/Privilege.java
@@ -0,0 +1,17 @@
+package com.proiect_refugiati_api.models;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
+public class Privilege {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "privileges")
+    private Collection<Role> roles;
+}
